ea821586a82cd08fbee91ed86070bee98d3da190
black0340 <black0340@naver.com>
2021-05-27 13:27:02 +0900

    'context fix'

--- raspi/live/views.py
+++ raspi/live/views.py
@@ -100,5 +100,5 @@ def login(request):
 
 def savedimg(request, imgNum):
     savedimage = SavedImage.objects.filter(ImageNumber=imgNum)
-    context = {'result':{'UltraSonic': savedimage[0]['UltraSonic'], 'ImageNumber':savedimage[0]['ImageNumber'],'CreatedAt':savedimage[0]['CreatedAt']}}
+    context = savedimage.values()[0]
     return render(request,'savedimg.html',context)
