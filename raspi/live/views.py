007b71a53160c1eb697ede78f08d863bdd639424
black0340 <black0340@naver.com>
2021-06-11 10:37:06 +0900

    fix

--- raspi/live/views.py
+++ raspi/live/views.py
@@ -6,11 +6,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import SavedImage
 from .serializers import SavedImageSerializer,MySerializer
-#import cv2
+import cv2
 import threading
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
-#import imutils
+import imutils
 import webbrowser
 import os
 
@@ -19,84 +19,84 @@ savetimer = 0
 distance = 0
 Trig = 11
 Echo = 12
-#GPIO.setmode(GPIO.BOARD)
-#GPIO.setup(Trig,GPIO.OUT)
-#GPIO.setup(Echo,GPIO.IN)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(Trig,GPIO.OUT)
+GPIO.setup(Echo,GPIO.IN)
 
 
-# def ImageSave(image, distance):
-#     imgCount = SavedImage.objects.count()
-#     cv2.imwrite(f'live/static/savedimage/{str(imgCount)}.png',image,params=[cv2.IMWRITE_PNG_COMPRESSION,0])
-#     savedimage = SavedImage(UltraSonic=distance,ImageNumber=imgCount)
-#     savedimage.save()
-#     return imgCount
+def ImageSave(image, distance):
+    imgCount = SavedImage.objects.count()
+    cv2.imwrite(f'live/static/savedimage/{str(imgCount)}.png',image,params=[cv2.IMWRITE_PNG_COMPRESSION,0])
+    savedimage = SavedImage(UltraSonic=distance,ImageNumber=imgCount)
+    savedimage.save()
+    return imgCount
 
-# def controlUltra():
+def controlUltra():
 
-#     global videosave,savetimer
-#     pulse_start=time.time()
-#     pulse_end =time.time()
-#     GPIO.setwarnings(False) 
-#     global distance
-#     GPIO.output(Trig,GPIO.LOW)
-#     time.sleep(0.005)
-#     GPIO.output(Trig,GPIO.HIGH)
-#     time.sleep(0.00001)
-#     GPIO.output(Trig,GPIO.LOW)
-#     while GPIO.input(Echo) == 0:
-#         pulse_start=time.time()
-#     while GPIO.input(Echo) == 1:
-#         pulse_end = time.time()
-#     pulse_duration = pulse_end - pulse_start
-#     distance = pulse_duration * 17000
-#     distance = round(distance,2)
-#     if distance>50 and savetimer >20:
-#         videosave = 1
-#         print(distance)
-#     savetimer += 1
-#     return distance
+    global videosave,savetimer
+    pulse_start=time.time()
+    pulse_end =time.time()
+    GPIO.setwarnings(False) 
+    global distance
+    GPIO.output(Trig,GPIO.LOW)
+    time.sleep(0.005)
+    GPIO.output(Trig,GPIO.HIGH)
+    time.sleep(0.00001)
+    GPIO.output(Trig,GPIO.LOW)
+    while GPIO.input(Echo) == 0:
+        pulse_start=time.time()
+    while GPIO.input(Echo) == 1:
+        pulse_end = time.time()
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17000
+    distance = round(distance,2)
+    if distance>50 and savetimer >20:
+        videosave = 1
+        print(distance)
+    savetimer += 1
+    return distance
     
-# class VideoCamera(object):
-#     def __init__(self):
-#         self.video = cv2.VideoCapture(0)
-#         (self.grabbed, self.frame) = self.video.read()
-#         threading.Thread(target=self.update, args=()).start()
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        (self.grabbed, self.frame) = self.video.read()
+        threading.Thread(target=self.update, args=()).start()
         
 
-#     def __del__(self):
-#         self.video.release()
+    def __del__(self):
+        self.video.release()
 
-#     def get_frame(self):
-#         image = self.frame
-#         _, jpeg = cv2.imencode('.jpg', image)
-#         return jpeg.tobytes()
+    def get_frame(self):
+        image = self.frame
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
 
-#     def update(self):
-#         global videosave,savetimer
-#         while True:
-#             threading.Thread(target=controlUltra).start()
-#             if videosave==1 and savetimer >20:
-#                 print('image saved')
-#                 videosave=0
-#                 savetimer =0
-#                 ImageSave(self.frame,distance)
-#             (self.grabbed, self.frame) = self.video.read()
-
-
-# def gen(camera):
-#     while True:
-#         frame = camera.get_frame()
-#         yield(b'--frame\r\n'
-#               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+    def update(self):
+        global videosave,savetimer
+        while True:
+            threading.Thread(target=controlUltra).start()
+            if videosave==1 and savetimer >20:
+                print('image saved')
+                videosave=0
+                savetimer =0
+                ImageSave(self.frame,distance)
+            (self.grabbed, self.frame) = self.video.read()
 
 
-# @gzip.gzip_page
-# def livefe(request):
-#     try: 
-#         cam = VideoCamera()
-#         return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
-#     except:  # This is bad! replace it with proper handling
-#         pass
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield(b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@gzip.gzip_page
+def livefe(request):
+    try: 
+        cam = VideoCamera()
+        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+    except:  # This is bad! replace it with proper handling
+        pass
 
 def streaming(request):
     return render(request,'index.html')
@@ -110,26 +110,26 @@ def savedimg(request, imgNum):
     print(context)
     return render(request,'savedimg.html',context)
 
-# @api_view(['GET'])
-# def dbserial(request):
-#     savedimage = SavedImage.objects.filter(ImageNumber=3)
-#     imgserial = SavedImageSerializer(savedimage[0])
-#     return Response(imgserial.data)
+@api_view(['GET'])
+def dbserial(request):
+    savedimage = SavedImage.objects.filter(ImageNumber=3)
+    imgserial = SavedImageSerializer(savedimage[0])
+    return Response(imgserial.data)
 
-# @api_view(['GET'])
-# def myserial(request):
-#     savedimage = SavedImage.objects.all()
-#     imgserial = MySerializer(savedimage,many=True)
-#     print(imgserial.data)
+@api_view(['GET'])
+def myserial(request):
+    savedimage = SavedImage.objects.all()
+    imgserial = MySerializer(savedimage,many=True)
+    print(imgserial.data)
     
-#     url = "https://www.youtube.com/embed/lL7YiUr7-8U?autoplay=1&mute=1"
-#     webbrowser.open(url)
+    url = "https://www.youtube.com/embed/lL7YiUr7-8U?autoplay=1&mute=1"
+    webbrowser.open(url)
     
-#     return Response(imgserial.data)
+    return Response(imgserial.data)
 
-# def videourl(request, url):
-#     openurl = "https://www.youtube.com/embed/"+url+"?autoplay=1&mute=1"
-#     webbrowser.open(openurl)
-#     return HttpResponse(url+" play")
+def videourl(request, url):
+    openurl = "https://www.youtube.com/embed/"+url+"?autoplay=1&mute=1"
+    webbrowser.open(openurl)
+    return HttpResponse(url+" play")
     
 
